8c4e4fe18bba5e30b2c6a5739e7a431c7e41fe94
Fabricio2103 <fabricioesp4@gmail.com>
2025-06-10 03:27:07 -0400

    guardar datos

--- modulos/utils/excel.py
+++ modulos/utils/excel.py
@@ -7,3 +7,10 @@ def crear_archivo_si_no_existe(nombre_archivo, encabezados):
         ws = wb.active
         ws.append(encabezados)
         wb.save(nombre_archivo)
+
+def guardar_en_excel(datos, nombre_archivo):
+    crear_archivo_si_no_existe(nombre_archivo, encabezados=[])
+    wb = openpyxl.load_workbook(nombre_archivo)
+    ws = wb.active
+    ws.append(datos)
+    wb.save(nombre_archivo)
