6f9e1489ff559b8b2b2b83f5375261c5c1c147f5
Fabricio2103 <fabricioesp4@gmail.com>
2025-06-12 07:00:55 -0400

    excel actualizado

--- modulos/backend/pacientes.py
+++ modulos/backend/pacientes.py
@@ -1,7 +1,7 @@
 from modulos.utils.excel import guardar_en_excel, crear_archivo_si_no_existe
 
 ARCHIVO = "pacientes.xlsx"
-ENCABEZADOS = ["Nombre", "Edad", "Género", "Historial Médico"]
+ENCABEZADOS = ["Nombre", "Edad", "Género"]
 
 crear_archivo_si_no_existe(ARCHIVO, ENCABEZADOS)
 
@@ -11,6 +11,5 @@ def guardar_paciente(datos):
         datos["nombre"],
         datos["edad"],
         datos["genero"],
-        datos.get("historial", "")  # Usa .get() por si no existe
     ]
     guardar_en_excel(datos_lista, ARCHIVO)
