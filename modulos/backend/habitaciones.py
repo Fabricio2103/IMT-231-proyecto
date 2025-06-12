07759d1ad335755efd2c35e54ba9a1af7eaf9e53
Fabricio2103 <fabricioesp4@gmail.com>
2025-06-12 09:07:18 -0400

    habitaciones arreglado

--- modulos/backend/habitaciones.py
+++ modulos/backend/habitaciones.py
@@ -1,15 +1,14 @@
 from modulos.utils.excel import guardar_en_excel, crear_archivo_si_no_existe
 
 ARCHIVO = "habitaciones.xlsx"
-ENCABEZADOS = ["Número", "Paciente Asignado", "Estado"]
+ENCABEZADOS = ["Paciente", "Habitacion"]
 
 crear_archivo_si_no_existe(ARCHIVO, ENCABEZADOS)
 
 def asignar_habitacion(datos):
     """Asigna habitación usando diccionario"""
     datos_lista = [
-        datos["numero"],
         datos["paciente"],
-        datos.get("estado", "Ocupada")  # Valor por defecto
+        datos["habitacion"],
     ]
     guardar_en_excel(datos_lista, ARCHIVO)
