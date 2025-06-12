fdf62756754b4decb637217defc70a9782766e90
Marcelo767n <luismarcelo767n@gmail.com>
2025-06-12 09:20:38 -0400

    update

--- modulos/backend/__init__.py
+++ modulos/backend/__init__.py
@@ -4,8 +4,5 @@ from .habitaciones import asignar_habitacion
 from .pacientes import guardar_paciente
 =======
 
-from .pacientes import guardar_paciente
-from .habitaciones import asignar_habitacion
-from .citas import programar_cita
 
 >>>>>>> 24bd2b2 (merge develop)
