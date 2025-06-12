3443589d45bffb572bdd0b165dbf9319dd0bca83
Marcelo767n <luismarcelo767n@gmail.com>
2025-06-12 01:56:48 -0400

    merge develop

--- modulos/backend/__init__.py
+++ modulos/backend/__init__.py
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
 from .citas import programar_cita
 from .habitaciones import asignar_habitacion
 from .pacientes import guardar_paciente
+=======
+
+from .pacientes import guardar_paciente
+from .habitaciones import asignar_habitacion
+from .citas import programar_cita
+
+>>>>>>> 24bd2b2 (merge develop)
